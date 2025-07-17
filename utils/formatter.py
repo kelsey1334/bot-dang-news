af86a82194cac386192555da1acf61dd458e1963
kelsey1334 <kelsey@procons.one>
2025-07-17 15:13:21 +0700

    Update formatter.py

--- utils/formatter.py
+++ utils/formatter.py
@@ -18,3 +18,9 @@ def format_headings_and_keywords(html, keyword):
     # Loại strong lồng nhau nếu có
     html = re.sub(r'(<strong>)+\s*(.*?)\s*(</strong>)+', r'<strong>\2</strong>', html)
     return html
+def clean_html_trailing_markdown(html):
+    # Xoá các ký tự #, * hoặc - dư thừa sau khi convert markdown sang HTML
+    html = re.sub(r'^\s*#+\s*', '', html, flags=re.MULTILINE)           # Xoá # đầu dòng
+    html = re.sub(r'^\s*[\*\-]\s*', '', html, flags=re.MULTILINE)       # Xoá * hoặc - đầu dòng
+    html = re.sub(r'(?<!<em>)(?<!<strong>)\*(?!</em>)(?!</strong>)', '', html)  # Xoá * thừa không nằm trong <em>/<strong>
+    return html
